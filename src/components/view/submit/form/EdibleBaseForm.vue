ab850edfe398afc691eb124bbbf28627cdd4066a
ChristiannYa <christian.ylopez02@gmail.com>
2026-05-27 16:40:16 -0500

    refactor(moved base form into its own component)

---
+++ src/components/view/submit/form/EdibleBaseForm.vue
@@ -0,0 +1,63 @@
+<script setup lang="ts">
+import { reactive, watch } from 'vue';
+import { buildEdibleBaseSchema, type EdibleBaseSchema } from "@/schemas/EdibleBaseSchema";
+import { useFormValidation } from '@/hooks/composables/formValidation';
+import EdibleBaseFormField from './EdibleBaseFormField.vue';
+import EdibleBaseServingUnitRadio from './EdibleBaseServingUnitRadio.vue';
+
+const emit = defineEmits<{
+	'validation-change': [isValid: boolean];
+	'set': [form: EdibleBaseSchema];
+}>();
+
+const form = reactive<EdibleBaseSchema>({
+	name: "",
+	brand: "",
+	servingUnit: "G",
+	amountPerServing: 0
+});
+
+const { 
+	focusedFields,
+	errorFields,
+	isValid,
+	buildFieldData
+} = useFormValidation(buildEdibleBaseSchema(), form);
+
+const nameFieldData = buildFieldData("Name", "Organic Hemp Seeds", "text");
+const brandFieldData = buildFieldData("Brand", "Kirkland Signature", "text");
+const amountPerServingFieldData = buildFieldData("Amount per serving", "60", "number");
+
+watch(isValid, (iv) => emit('validation-change', iv), { immediate: true })
+watch(form, (f) => emit('set', { ...f }), { deep: true });
+</script>
+
+<template>
+	<div class="flex flex-col gap-y-4">
+		<EdibleBaseFormField
+			v-model="form.name"
+			:input-data="nameFieldData.field"
+			:is-focused="focusedFields['name'] === true"
+			:error-message="undefined"
+			@reset="nameFieldData.deleteError"
+		/>
+		<EdibleBaseFormField
+			v-model="form.brand"
+			:input-data="brandFieldData.field"
+			:is-focused="focusedFields['brand'] === true"
+			:errorMessage="undefined"
+			@reset="brandFieldData.deleteError"
+		/>
+		<EdibleBaseFormField
+			v-model="form.amountPerServing"
+			:input-data="amountPerServingFieldData.field"
+			:is-focused="focusedFields['amount per serving'] === true"
+			:errorMessage="undefined"
+			@reset="amountPerServingFieldData.deleteError"
+		/>
+		<EdibleBaseServingUnitRadio 
+			v-model="form.servingUnit"
+			:ref="errorFields.servingUnit"
+		/>
+	</div>
+</template>
--- src/views/SubmitView.vue
+++ src/views/SubmitView.vue
@@ -1,35 +1,18 @@
 <script setup lang="ts">
 import View from "@/components/shared/View.vue";
 import SubmissionHeader from "@/components/view/submit/header/SubmissionHeader.vue";
-import { reactive, ref } from "vue";
-import { buildEdibleBaseSchema, type EdibleBaseSchema } from "@/schemas/EdibleBaseSchema";
-import EdibleBaseFormField from "@/components/view/submit/form/EdibleBaseFormField.vue";
-import EdibleBaseServingUnitRadio from "@/components/view/submit/form/EdibleBaseServingUnitRadio.vue";
-import { useFormValidation } from "@/hooks/composables/formValidation";
+import { ref } from "vue";
+import EdibleBaseForm from "@/components/view/submit/form/EdibleBaseForm.vue";
+import type { EdibleBaseSchema } from "@/schemas/EdibleBaseSchema";
 
 const currentStep = ref(1);
 
-const baseForm = reactive<EdibleBaseSchema>({
-	name: "",
-	brand: "",
-	servingUnit: "G",
-	amountPerServing: 0
-});
+const isBaseFormValid = ref(false);
 
-const { 
-	focusedFields,
-	errorFields,
-	isValid: isStep1Valid,
-	buildFieldData
-} = useFormValidation(buildEdibleBaseSchema(), baseForm);
-
-const nameFieldData = buildFieldData("Name", "Organic Hemp Seeds", "text");
-const brandFieldData = buildFieldData("Brand", "Kirkland Signature", "text");
-const amountPerServingFieldData = buildFieldData("Amount per serving", "60", "number");
+const baseForm = ref<EdibleBaseSchema | null>(null);
 
 async function onSubmit() {
 	console.log("Submitting form");
-	console.log(JSON.stringify(baseForm, null, 2));
 };
 
 function onPrev() {
@@ -55,40 +38,16 @@ function onNext() {
 		<div class="flex flex-col gap-4">
 			<SubmissionHeader 
 				:current-step="currentStep"
-				:isNextDisabled="!isStep1Valid"
+				:isNextDisabled="!isBaseFormValid"
 				@prev="onPrev" 
 				@next="onNext"
 			/>
-	
-			<div v-show="currentStep === 1">
-				<form class="flex flex-col gap-y-4">
-					<EdibleBaseFormField
-						v-model="baseForm.name"
-						:input-data="nameFieldData.field"
-						:is-focused="focusedFields['name'] === true"
-						:errorMessage="undefined"
-						@reset="nameFieldData.deleteError"
-					/>
-					<EdibleBaseFormField
-						v-model="baseForm.brand"
-						:input-data="brandFieldData.field"
-						:is-focused="focusedFields['brand'] === true"
-						:errorMessage="undefined"
-						@reset="brandFieldData.deleteError"
-					/>
-					<EdibleBaseFormField
-						v-model="baseForm.amountPerServing"
-						:input-data="amountPerServingFieldData.field"
-						:is-focused="focusedFields['amount per serving'] === true"
-						:errorMessage="undefined"
-						@reset="amountPerServingFieldData.deleteError"
-					/>
-					<EdibleBaseServingUnitRadio 
-						v-model="baseForm.servingUnit"
-						:ref="errorFields.servingUnit"
-					/>
-				</form>
-			</div>
+
+			<EdibleBaseForm 
+				v-show="currentStep === 1"
+				@validation-change="isBaseFormValid = $event"
+				@set="baseForm = $event"
+			/>
 		</div>
 	</View>
 </template>
