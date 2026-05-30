1f8641de0f1e60cd8dd65cd7d86ac315da43bf3f
ChristiannYa <christian.ylopez02@gmail.com>
2026-05-30 17:33:03 -0500

    chore(implemented shared dev script for local network access)

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,19 @@ import vue from "@vitejs/plugin-vue";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 import { APP_PORT, PROXY_PORT } from "./src/config/config";
-// import basicSsl from '@vitejs/plugin-basic-ssl'
+import basicSsl from '@vitejs/plugin-basic-ssl'
+
+const isShared = process.env.VITE_SHARED === "true";
 
 export default defineConfig({
 	plugins: [
 		vue(), 
 		tailwindcss(), 
-		// basicSsl()
+		isShared && basicSsl()
 	],
 	server: {
 		port: APP_PORT,
-		host: false,
+		host: isShared,
 		proxy: {
 			"/pxy": {
 				target: `http://localhost:${PROXY_PORT}`,
