54b248e6cc3a5b34cd368ab03c994180298d3b6f
ChristiannYa <christian.ylopez02@gmail.com>
2026-06-18 14:04:56 -0500

    fix(router view layout)

--- src/App.vue
+++ src/App.vue
@@ -6,11 +6,9 @@ const authStore = useAuthStore();
 </script>
 
 <template>
-	<div class="w-screen min-h-screen relative">
-		<RouterView />
-		<Spinner 
-			v-if="authStore.isAuthLoading" 
-			class="absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2"
-		/>
-	</div>
+	<RouterView />
+	<Spinner 
+		v-if="authStore.isAuthLoading" 
+		class="absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2"
+	/>
 </template>
